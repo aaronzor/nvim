fc4491a7e08318c62875a1fed31d3daa52c40614
aaronzor <mcmaaron@gmail.com>
2023-02-17 22:57:16 +0000

    Update lazy.lua

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -351,9 +351,9 @@ local opts = {
   server = {
     on_attach = function(_, bufnr)
       -- Hover actions
-      vim.keymap.set("n", "<Leader>A", rt.hover_actions.hover_actions, { buffer = bufnr })
+      vim.keymap.set("n", "<Leader>A", require("rust-tools").hover_actions.hover_actions, { buffer = bufnr })
       -- Code action groups
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+      vim.keymap.set("n", "<Leader>a", require("rust-tools").code_action_group.code_action_group, { buffer = bufnr })
     end,
   },
 
